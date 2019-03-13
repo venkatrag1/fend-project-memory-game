dbdef70aae8ceabb011c0aef24a862708a8d8d19
venkatrag1 <venkatraghavan.r@gmail.com>
2019-03-13 06:51:54 +0000

    off to a start

--- js/app.js
+++ js/app.js
@@ -1,7 +1,8 @@
 /*
  * Create a list that holds all of your cards
  */
-
+let cards = ['fa-diamond', 'fa-paper-plane-o', 'fa-anchor', 'fa-bolt',
+            'fa-leaf'];
 
 /*
  * Display the cards on the page
@@ -9,10 +10,23 @@
  *   - loop through each card and create its HTML
  *   - add each card's HTML to the page
  */
+function startGame() {
+    const deck = document.querySelector('.deck');
+    deck.innerHTML = '';
+    for (const card of cards) {
+        const cardli = document.createElement('li');
+        cardli.classList.add('card');
+        cardli.classList.add('open');
+        cardli.classList.add('show');
+        cardli.innerHTML = `<i class="fa ${card}"></i>`;
+        deck.appendChild(cardli);
+    }
+}
+
 
 // Shuffle function from http://stackoverflow.com/a/2450976
 function shuffle(array) {
-    var currentIndex = array.length, temporaryValue, randomIndex;
+    let currentIndex = array.length, temporaryValue, randomIndex;
 
     while (currentIndex !== 0) {
         randomIndex = Math.floor(Math.random() * currentIndex);
@@ -36,3 +50,6 @@ function shuffle(array) {
  *    + increment the move counter and display it on the page (put this functionality in another function that you call from this one)
  *    + if all cards have matched, display a message with the final score (put this functionality in another function that you call from this one)
  */
+
+
+startGame();
